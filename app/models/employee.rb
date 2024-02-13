c11ac2e1816ba4eea6edf929cd28fb80bec6cfe1
Dennis N. Every <n.every@me.com>
2024-02-13 11:23:21 -0400

    Add search

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,8 @@ class Employee < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     %w[first_name last_name phone email birthday country active]
   end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
